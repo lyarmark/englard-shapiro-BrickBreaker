c39f73ceca0c0ec044d2a7f1d441c20f8537818d
unknown <chavs915@gmail.com>
2016-03-05 20:46:00 -0500

    added all bricks

--- src/main/java/englard_shapiro_brickBreaker/englard_shapiro_brickBreaker/Board.java
+++ src/main/java/englard_shapiro_brickBreaker/englard_shapiro_brickBreaker/Board.java
@@ -2,6 +2,7 @@ package englard_shapiro_brickBreaker.englard_shapiro_brickBreaker;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -10,7 +11,7 @@ public class Board extends JPanel {
 
 	private Paddle paddle;
 	private Ball ball;
-	private Piece brick;
+	private ArrayList<Piece> bricks;
 	public static final int BOARD_HEIGHT = 600;
 	public static final int BOARD_WIDTH = 600;
 	private int livesUsed;
@@ -21,7 +22,67 @@ public class Board extends JPanel {
 		paddle = new Paddle();
 		ball = new Ball(BOARD_WIDTH / 2,
 				(paddle.getY() - Paddle.PADDLE_HEIGHT) - 10);
-		brick = new Piece(100, 100, Color.RED);
+		bricks = new ArrayList<Piece>();
+		bricks.add(new Piece(0, 50, Color.RED));
+		bricks.add(new Piece(50, 50, Color.RED));
+		bricks.add(new Piece(100, 50, Color.RED));
+		bricks.add(new Piece(150, 50, Color.RED));
+		bricks.add(new Piece(200, 50, Color.RED));
+		bricks.add(new Piece(250, 50, Color.RED));
+		bricks.add(new Piece(300, 50, Color.RED));
+		bricks.add(new Piece(350, 50, Color.RED));
+		bricks.add(new Piece(400, 50, Color.RED));
+		bricks.add(new Piece(450, 50, Color.RED));
+		bricks.add(new Piece(500, 50, Color.RED));
+		bricks.add(new Piece(550, 50, Color.RED));
+		bricks.add(new Piece(0, 80, Color.ORANGE));
+		bricks.add(new Piece(50, 80, Color.ORANGE));
+		bricks.add(new Piece(100, 80, Color.ORANGE));
+		bricks.add(new Piece(150, 80, Color.ORANGE));
+		bricks.add(new Piece(200, 80, Color.ORANGE));
+		bricks.add(new Piece(250, 80, Color.ORANGE));
+		bricks.add(new Piece(300, 80, Color.ORANGE));
+		bricks.add(new Piece(350, 80, Color.ORANGE));
+		bricks.add(new Piece(400, 80, Color.ORANGE));
+		bricks.add(new Piece(450, 80, Color.ORANGE));
+		bricks.add(new Piece(500, 80, Color.ORANGE));
+		bricks.add(new Piece(550, 80, Color.ORANGE));
+		bricks.add(new Piece(0, 110, Color.YELLOW));
+		bricks.add(new Piece(50, 110, Color.YELLOW));
+		bricks.add(new Piece(100, 110, Color.YELLOW));
+		bricks.add(new Piece(150, 110, Color.YELLOW));
+		bricks.add(new Piece(200, 110, Color.YELLOW));
+		bricks.add(new Piece(250, 110, Color.YELLOW));
+		bricks.add(new Piece(300, 110, Color.YELLOW));
+		bricks.add(new Piece(350, 110, Color.YELLOW));
+		bricks.add(new Piece(400, 110, Color.YELLOW));
+		bricks.add(new Piece(450, 110, Color.YELLOW));
+		bricks.add(new Piece(500, 110, Color.YELLOW));
+		bricks.add(new Piece(550, 110, Color.YELLOW));
+		bricks.add(new Piece(0, 140, Color.GREEN));
+		bricks.add(new Piece(50, 140, Color.GREEN));
+		bricks.add(new Piece(100, 140, Color.GREEN));
+		bricks.add(new Piece(150, 140, Color.GREEN));
+		bricks.add(new Piece(200, 140, Color.GREEN));
+		bricks.add(new Piece(250, 140, Color.GREEN));
+		bricks.add(new Piece(300, 140, Color.GREEN));
+		bricks.add(new Piece(350, 140, Color.GREEN));
+		bricks.add(new Piece(400, 140, Color.GREEN));
+		bricks.add(new Piece(450, 140, Color.GREEN));
+		bricks.add(new Piece(500, 140, Color.GREEN));
+		bricks.add(new Piece(550, 140, Color.GREEN));
+		bricks.add(new Piece(0, 170, Color.BLUE));
+		bricks.add(new Piece(50, 170, Color.BLUE));
+		bricks.add(new Piece(100, 170, Color.BLUE));
+		bricks.add(new Piece(150, 170, Color.BLUE));
+		bricks.add(new Piece(200, 170, Color.BLUE));
+		bricks.add(new Piece(250, 170, Color.BLUE));
+		bricks.add(new Piece(300, 170, Color.BLUE));
+		bricks.add(new Piece(350, 170, Color.BLUE));
+		bricks.add(new Piece(400, 170, Color.BLUE));
+		bricks.add(new Piece(450, 170, Color.BLUE));
+		bricks.add(new Piece(500, 170, Color.BLUE));
+		bricks.add(new Piece(550, 170, Color.BLUE));
 		livesUsed = 0;
 		this.frame = frame;
 	}
@@ -35,9 +96,14 @@ public class Board extends JPanel {
 		g.fillOval(ball.getX(), ball.getY(), Ball.BALL_DIAMETER,
 				Ball.BALL_DIAMETER);
 		// create loop to set up all pieces - make 2d-array of pieces
-		g.setColor(brick.getColor());
-		g.fillRect(brick.getX(), brick.getY(), brick.getLength(),
-				brick.getWidth());
+		for (Piece brick : bricks) {
+			g.setColor(brick.getColor());
+			g.fillRect(brick.getX(), brick.getY(), brick.getLength(),
+					brick.getWidth());
+			g.setColor(Color.BLACK);
+			g.drawRect(brick.getX(), brick.getY(), brick.getLength(),
+					brick.getWidth());
+		}
 	}
 
 	public void movePaddleLeft() {
@@ -73,7 +139,7 @@ public class Board extends JPanel {
 
 			livesUsed++;
 		} else {
-			ball.move(paddle.getX(), paddle.getY(), brick);
+			ball.move(paddle.getX(), paddle.getY(), bricks);
 		}
 	}
 
